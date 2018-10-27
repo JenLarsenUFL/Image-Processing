2db2daba85950e7cf56d13bb851b0c3bc5f5e5b7
FeelingMathy <the.schroedinger.cat@gmail>
2018-10-27 00:03:52 -0400

    Add project files.

---
+++ Image Processing/main.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+
+struct Header
+{
+	char idLength;
+	char colorMapType;
+	char dataTypeCode;
+	short colorMapOrigin;
+	short colorMapLength;
+	char colorMapDepth;
+	short xOrigin;
+	short yOrigin;
+	short width;
+	short height;
+	char bitsPerPixel;
+	char imageDescriptor;
+};
+//*****accessors*****
+
+
+
+int main() 
+{
+
+}
